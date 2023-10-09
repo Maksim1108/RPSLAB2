67b094c8db508048ef89045ba5da1931e2b159c1
Maksim Lyanko <maksim.lyanko@bk.ru>
2023-10-09 13:40:41 +0300

    RPS Lab 2

---
+++ config/path.js
@@ -0,0 +1,4 @@
+// Переменная хранящая в себе путь к тестовому файлу
+const FILE_PATH = "C:\\Users\\Максим\\Desktop\\iloveubro.txt"
+
+module.exports = FILE_PATH
---
+++ fileWork/fileWork.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+// Функция для чтения данных из файла и передачи их в виде массива чисел в функцию обратного вызова
+const readFromFile = (filePath, callback) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            // В случае ошибки выводим сообщение и передаем ошибку в функцию обратного вызова
+            console.error('Произошла ошибка при чтении файла:', err);
+            callback(err, null);
+            return;
+        }
+
+        // Разбиваем данные на массив чисел, используя пробелы как разделители
+        const arr = data.split(/\s+/).map(Number);
+
+        // Фильтруем NaN значения (если они есть)
+        const filteredArr = arr.filter(num => !isNaN(num));
+
+        // Вызываем функцию обратного вызова, передавая отфильтрованный массив чисел
+        callback(null, filteredArr);
+    });
+}
+
+// Экспорт модуля fs и функции readFromFile для использования в других модулях
+module.exports = { fs, readFromFile };
