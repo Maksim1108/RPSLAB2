4732a052d49ebd6888dc81e4da1427f14607065c
rewtrywer <147376422+rewtrywer@users.noreply.github.com>
2023-12-18 17:28:56 +0700

    Update testArrayMode.js
    comments added

--- server/models/testArrayMode.js
+++ server/models/testArrayMode.js
@@ -1,5 +1,7 @@
+// Подключаем необходимую библиотеку
 const mongoose = require('mongoose');
 
+// Создаем схему для модели
 const TestArraySchema = new mongoose.Schema(
     {
         numberOfArrays: Number,
@@ -10,6 +12,8 @@ const TestArraySchema = new mongoose.Schema(
     },
 );
 
+// Создаем модель, используя созданную схему
 const testArrayModel = mongoose.model('TestArrays', TestArraySchema);
 
+// Экспортируем модель для использования в других файлах
 module.exports = testArrayModel
