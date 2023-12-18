87a42cfd46e0501bc5b1a8a2b3817d04abc45850
rewtrywer <147376422+rewtrywer@users.noreply.github.com>
2023-12-18 17:16:32 +0700

    Update algoritmhs.js
    comments added

--- server/algoritmhs/algoritmhs.js
+++ server/algoritmhs/algoritmhs.js
@@ -1,10 +1,15 @@
+// Функция getRandomInt возвращает случайное целое число в заданном диапазоне
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Функция addArraysToDB генерирует указанное количество массивов случайных размеров
+// и заполняет их случайными числами, затем возвращает массив этих массивов
 const addArraysToDB = (numberOfArrays) => {
+     // Массив для хранения сгенерированных массивов
     const arrayOfArrays = []
 
+     // Цикл для создания указанного числа массивов
     for (let i = 0; i < numberOfArrays; i++) {
         // Генерируем случайный размер массива от 1 до 10 (включительно)
         let arraySize = getRandomInt(1, 10);
@@ -19,7 +24,9 @@ const addArraysToDB = (numberOfArrays) => {
         arrayOfArrays.push(newArray);
     }
 
+     // Возвращаем сгенерированный массив массивов
     return arrayOfArrays
 }
 
+// Экспортируем функцию addArraysToDB для использования в других модулях
 module.exports = addArraysToDB
